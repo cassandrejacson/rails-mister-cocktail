543bd8f7931c39b64088cc720d66d27ab3403019
cassandre jacson <cassandre.jacson@edhec.com>
2018-10-18 21:26:13 +0200

    partie 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: "controller#action"
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Yukon")
+Ingredient.create(name: "Green Chartreuse")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "strawberry")
+Cocktail.create(name: "mojito")
